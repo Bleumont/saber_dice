8a0cc415abb26afd0d47c10f38e3f86f0e877d94
Bleumont <fede.inch@gmail.com>
2021-02-19 15:22:24 -0300

    roll btn style & deleted comments

--- scripts.js
+++ scripts.js
@@ -27,6 +27,7 @@ document.addEventListener('click', (e) => {
     resultBox.innerHTML = `You got <strong>${calculateScore()}</strong> points!`;
   }
 });
+
 const numToCssClass = (number) => {
   return number === 1
     ? 'one'
@@ -91,46 +92,3 @@ const calculateScore = () => {
 //     );
 //   });
 // });
-
-// function numToCssClass(number) {
-//   return number === 1
-//     ? 'one'
-//     : number === 2
-//     ? 'two'
-//     : number === 3
-//     ? 'three'
-//     : number === 4
-//     ? 'four'
-//     : number === 5
-//     ? 'five'
-//     : number === 6
-//     ? 'six'
-//     : 'err';
-// }
-// function roll() {
-//   return Math.floor(Math.random() * 6 + 1);
-// }
-// function getDiceValues() {
-//   values = [];
-//   diceElements = document.querySelectorAll('.dice');
-//   diceElements.forEach((el) => {
-//     values.push(el.dataset.value);
-//   });
-//   return values;
-// }
-// function normalScore(diceValue) {
-//   return diceValue == 1 ? 100 : diceValue == 5 ? 50 : 0;
-// }
-// function calculateScore() {
-//   const [dice1, dice2, dice3] = getDiceValues();
-
-//   let score = 0;
-//   [...getDiceValues()].forEach((el) => {
-//     score += normalScore(el);
-//   });
-//   return +dice1 + +dice2 + +dice3 == 3
-//     ? 1000
-//     : +dice1 == +dice2 && +dice2 == +dice3
-//     ? +dice1 * 100
-//     : score;
-// }
