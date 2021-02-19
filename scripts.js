e5354fe31ffc4c0c7a84a0c97dbda20e6afbf4b7
Bleumont <fede.inch@gmail.com>
2021-02-19 15:12:57 -0300

    added jquery algorithm(commented)

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+//////////////////////////
+// Vanilla js algorithm //
+//////////////////////////
+
 document.addEventListener('click', (e) => {
   const diceList = document.querySelectorAll('.dice');
   const resultBox = document.getElementById('result');
@@ -23,7 +27,7 @@ document.addEventListener('click', (e) => {
     resultBox.innerHTML = `You got <strong>${calculateScore()}</strong> points!`;
   }
 });
-function numToCssClass(number) {
+const numToCssClass = (number) => {
   return number === 1
     ? 'one'
     : number === 2
@@ -37,22 +41,22 @@ function numToCssClass(number) {
     : number === 6
     ? 'six'
     : 'err';
-}
-function roll() {
+};
+const roll = () => {
   return Math.floor(Math.random() * 6 + 1);
-}
-function getDiceValues() {
+};
+const getDiceValues = () => {
   values = [];
   diceElements = document.querySelectorAll('.dice');
   diceElements.forEach((el) => {
     values.push(el.dataset.value);
   });
   return values;
-}
+};
 const normalScore = (diceValue) => {
   return diceValue == '1' ? 100 : diceValue == '5' ? 50 : 0;
 };
-function calculateScore() {
+const calculateScore = () => {
   const [dice1, dice2, dice3] = getDiceValues();
 
   let score = 0;
@@ -65,4 +69,68 @@ function calculateScore() {
     : +dice1 == +dice2 && +dice2 == +dice3
     ? +dice1 * 100
     : score;
-}
+};
+/////////////////////////////////////
+// Same algorithm but with jQuery! //
+/////////////////////////////////////
+
+// $('#button-roll').click(function () {
+//   const dice = $('.dice');
+//   dice.toggleClass('dice-rotate');
+//   dice.each(function () {
+//     diceValue = roll();
+//     $(this).attr('data-value', diceValue);
+//     diceClass = numToCssClass(+diceValue);
+//     $(this)
+//       .children('.dice-point')
+//       .removeClass(['one', 'two', 'three', 'four', 'five', 'six'])
+//       .toggleClass(diceClass);
+
+//     $('#result').html(
+//       "You've got <strong>" + calculateScore() + '</strong> points!'
+//     );
+//   });
+// });
+
+// function numToCssClass(number) {
+//   return number === 1
+//     ? 'one'
+//     : number === 2
+//     ? 'two'
+//     : number === 3
+//     ? 'three'
+//     : number === 4
+//     ? 'four'
+//     : number === 5
+//     ? 'five'
+//     : number === 6
+//     ? 'six'
+//     : 'err';
+// }
+// function roll() {
+//   return Math.floor(Math.random() * 6 + 1);
+// }
+// function getDiceValues() {
+//   values = [];
+//   diceElements = document.querySelectorAll('.dice');
+//   diceElements.forEach((el) => {
+//     values.push(el.dataset.value);
+//   });
+//   return values;
+// }
+// function normalScore(diceValue) {
+//   return diceValue == 1 ? 100 : diceValue == 5 ? 50 : 0;
+// }
+// function calculateScore() {
+//   const [dice1, dice2, dice3] = getDiceValues();
+
+//   let score = 0;
+//   [...getDiceValues()].forEach((el) => {
+//     score += normalScore(el);
+//   });
+//   return +dice1 + +dice2 + +dice3 == 3
+//     ? 1000
+//     : +dice1 == +dice2 && +dice2 == +dice3
+//     ? +dice1 * 100
+//     : score;
+// }
